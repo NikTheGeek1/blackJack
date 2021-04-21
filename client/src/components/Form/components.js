8855e480131e6a905bdd827806b1bd5d70b362ae
nikthegeek1 <ntheodoropoulos@outlook.com>
2021-04-21 16:36:18 +0300

    added custom select component

--- client/src/components/Form/components.js
+++ client/src/components/Form/components.js
@@ -1,6 +1,7 @@
 import './FormInput.css';
 import './FormButton.css';
 import './FormSelect.css';
+import { useEffect, useState } from 'react';
 
 export const FormInput = ({ type, title, value, onChange, required, otherProps }) => {
     return (
@@ -20,35 +21,66 @@ export const FormInput = ({ type, title, value, onChange, required, otherProps }
     );
 };
 
-export const FormButton = ({ title, otherProps, extraClasses}) => {
+export const FormButton = ({ title, otherProps, extraClasses }) => {
     return (
         <input type="submit" value={title} {...otherProps} className={`form-button ${extraClasses}`} />
     );
 };
 
-export const Button = ({ title, otherProps, extraClasses, onClick}) => {
+export const Button = ({ title, otherProps, extraClasses, onClick }) => {
     return (
         <button onClick={onClick} {...otherProps} className={`form-button ${extraClasses}`}>{title}</button>
     );
 };
 
 
+export const FormSelect = ({ otherProps, onChange, defaultValue, value, valuesArray }) => {
+    const [activeClass, setActiveClass] = useState("");
+    const [displayOptionsClass, setDisplayOptionsClass] = useState("");
+    const [selectedOption, setSelectedOption] = useState(defaultValue);
 
+    useEffect(() => {
+        document.addEventListener("click", outterClickHandler);
+        return () => {
+            document.removeEventListener("click", outterClickHandler);
+        }
+    }, [activeClass]);
 
-export const FormSelect = ({ otherProps, onChange, children, defaultValue, value }) => {
+    const outterClickHandler = () => {
+        if (activeClass) {
+            setActiveClass("");
+            setDisplayOptionsClass("");
+        } 
+    };
 
+    const toggleActiveHandler = () => {
+        if (!activeClass) {
+            setDisplayOptionsClass("select-options-display")
+            setActiveClass("active");
+        }
+    };
+
+    const clickOptionHandler = e => {
+        onChange(e);
+        setSelectedOption(e.target.getAttribute('rel'));
+    };
+
+    const valuesLI = valuesArray.map(v => <li onClick={e => clickOptionHandler(e)} className="select-li" key={v} rel={v}>{v}</li>);
+    const valuesOptions = valuesArray.map(v => <option key={v} value={v}>{v}</option>);
     return (
-        <select onChange={onChange} {...otherProps} value={value}>
-            <option value="" disabled>{defaultValue}</option>
-            {children}
-        </select>
+        <div className="select">
+            <select className="select-hidden" onChange={onChange} {...otherProps} value={value}>
+                <option className="select-option" value="hide">-- {defaultValue} --</option>
+                {valuesOptions}
+            </select>
+            <div className={"select-styled " + activeClass} onClick={toggleActiveHandler}>
+                {selectedOption}
+            </div>
+            <ul className={"select-options " + displayOptionsClass}>
+                <li className="select-li" rel="hide">-- {defaultValue} --</li>
+                {valuesLI}
+            </ul>
+        </div>
     );
 };
 
-
-export const FormOption = ({ title }) => {
-
-    return (
-            <option value={title}>{title}</option>
-    );
-};
--- client/src/components/NewMatch/NewMatch.js
+++ client/src/components/NewMatch/NewMatch.js
@@ -5,8 +5,8 @@ import { useState } from 'react';
 const NewMatch = ({ addMatchHandler }) => {
     const [matchName, setMatchName] = useState('');
     const [maxNumberOfPlayers, setMaxNumberOfPlayers] = useState('Maximum number of players: ');
-    const [privacy, setPrivacy] = useState('');
-    const [gameType, setGameType] = useState('');
+    const [privacy, setPrivacy] = useState('hide');
+    const [gameType, setGameType] = useState('hide');
     const [matchPassword, setMatchPassword] = useState('');
 
     const submitFormHandler = e => {
@@ -30,22 +30,24 @@ const NewMatch = ({ addMatchHandler }) => {
     return (
         <div className="new-match-container">
             <form onSubmit={submitFormHandler} className="new-match-form">
-                <FormInput title="Game name: " type="text" onChange={(e) => setMatchName(e.target.value)} value={matchName} required />
-                <FormInput title="Maximum number of players: " type="number" onChange={e => setMaxNumberOfPlayers(e.target.value)} value={maxNumberOfPlayers} otherProps={{ min: 2, max: 7 }} required />
+                <FormInput title="-- Game name --" type="text" onChange={(e) => setMatchName(e.target.value)} value={matchName} required />
+                <FormInput title="-- Maximum number of players --" type="number" onChange={e => setMaxNumberOfPlayers(e.target.value)} value={maxNumberOfPlayers} otherProps={{ min: 2, max: 7 }} required />
                 <div>
-                <FormSelect defaultValue="Privacy" onChange={e => setPrivacy(e.target.value)} value={privacy}>
-                    <FormOption title="Private" />
-                    <FormOption title="Public" />
-                </FormSelect>
+                    <FormSelect defaultValue="-- Privacy --"
+                        onChange={e => setPrivacy(e.target.getAttribute('rel'))}
+                        value={privacy}
+                        valuesArray={["Private", "Public"]}
+                    />
                 </div>
                 {privacy === "Private" &&
                     <FormInput title="Password: " type="text" onChange={(e) => setMatchPassword(e.target.value)} value={matchPassword} required />
                 }
                 <div>
-                <FormSelect defaultValue="Type" onChange={e => setGameType(e.target.value)} value={gameType}>
-                    <FormOption title="Computer" />
-                    <FormOption title="Humans" />
-                </FormSelect>
+                    <FormSelect defaultValue="-- Type --"
+                        onChange={e => setGameType(e.target.getAttribute('rel'))}
+                        value={gameType}
+                        valuesArray={["Computer", "Humans"]}
+                    />
                 </div>
                 <FormButton title="Create game" />
             </form>
